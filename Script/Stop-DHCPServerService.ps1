3f45921104ac74f2d1e9ae76fd8f2e1a40e0825c
shahin4s <shahin.patwary@gmail.com>
2020-12-08 13:04:31 +0600

    Create Stop-DHCPServerService.ps1

---
+++ Script/Stop-DHCPServerService.ps1
@@ -0,0 +1,17 @@
+﻿#let's try with the Stop DHCP servier's service
+#Shahin 06.Dec.20
+#this script add in task scheduler 
+
+$DService = 'DHCPServer'
+$Server = 'wds02'
+# now look at things
+if ((Get-Service 'DHCPServer').status -ne "stopped")
+{
+   Stop-Service $DService 
+   Start-Sleep -Seconds 15
+   Write-Host -foregroundcolor Green -Object "The service stopped"
+}
+else
+{
+  Write-host -foregroundcolor Yellow -object "Already Stopped"
+}
