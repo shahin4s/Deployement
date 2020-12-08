5a9da0d6c1a0fb2348c4b10976a54756c309aca1
shahin4s <shahin.patwary@gmail.com>
2020-12-08 13:20:06 +0600

    Create Create-LocaladminAccount.ps1

---
+++ Script/Create-LocaladminAccount.ps1
@@ -0,0 +1,12 @@
+﻿# When used in a MDT task sequence, copy the script to deployment share / scripts folder,
+# and use the below command line:
+# Powershell.exe -ExecutionPolicy ByPass -File "%SCRIPTROOT%\Create_localadmin_account.ps1"
+
+#Enable Remotesigne
+set-executionpolicy remotesigned 
+$username=hostname | %{ $_.Split('-')[0]; }
+#$password=Read-Host -AsSecureString
+$newuser=New-LocalUser -Name $username.ToLower() -NoPassword 
+Add-LocalGroupMember -Name administrators -Member $newuser
+#disable RemoteSigne
+Set-ExecutionPolicy Restricted
