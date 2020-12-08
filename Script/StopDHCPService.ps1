8c8f69a87bd33d06ed9a698bcccb6fb58b40281e
shahin4s <shahin.patwary@gmail.com>
2020-12-08 13:20:01 +0600

    Create StopDHCPService.ps1

---
+++ Script/StopDHCPService.ps1
@@ -0,0 +1,16 @@
+﻿#Srop DHCP service on wds02
+
+
+$servicename = 'DHCPServer'
+$arrService = Get-Service -Name $ServiceName
+
+while ($arrService.Status -eq 'Running')
+{
+    Stop-Service $servicename
+    Write-Host $arrService.Status
+    Write-Host 'service stopping'
+    $arrService.Refresh()
+    if ($arrService.Status -ne 'running'){
+    Write-Host 'service is not running'
+    }
+}
